e9e861788ec731c69d140e01db84b704f16e2886
Tom.Schwarzburg <tom.schwarzburg@stuudent.hpi.de>
2018-11-06 16:59:09 +0100

    Fix #20

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,4 +4,6 @@ class Paper < ApplicationRecord
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
+
+  scope :year, ->(year) {where("year IS ?", year)}
 end
