ed097e823e09a3b493e6c2ce10863af4f1a2cb3d
Tom.Schwarzburg <tom.schwarzburg@stuudent.hpi.de>
2018-11-06 14:16:32 +0100

    Add test for #11

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 describe "Edit author page", type: :feature do
+  before do
+    @author = FactoryBot.create :author
+  end
 
   it "should render withour error" do
-    @author = FactoryBot.create :author
-    puts page.html
+    visit edit_author_path(@author)
   end
+
+  it "should update author's information" do
+    new_first_name = "Alan Mathison"
+    visit edit_author_path(@author)
+    page.fill_in 'author[first_name]', with: new_first_name
+    find('input[type="submit"]').click
+
+    author.reload
+    expect(@author.first_name).to be new_first_name
 
 end
