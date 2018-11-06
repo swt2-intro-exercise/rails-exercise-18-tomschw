d746265562def56864b27238bc46f6c512728a70
Tom.Schwarzburg <tom.schwarzburg@stuudent.hpi.de>
2018-11-06 14:11:54 +0100

    Add test #10

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
+
+  it "should render withour error" do
+    @author = FactoryBot.create :author
+    visit edit_author_path(@author)
+  end
+
+end
